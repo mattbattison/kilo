98890b446e32cb86422ba0d73a45e3797ed368ce
Matt Battison <matt.battison@gmail.com>
2019-08-07 22:18:49 +0100

    Step 74

--- kilo.c
+++ kilo.c
@@ -304,9 +304,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1) {
-                E.cx++;
-            }
+            E.cx++;
             break;
         case ARROW_LEFT:
             if (E.cx != 0) {
